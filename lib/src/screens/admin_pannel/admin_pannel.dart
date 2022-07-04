f6c480ad9e6d521ef85a6a0fe9effd7e4fa720cf
Tooba Tariq <toobatariq3333@gmail.com>
2022-07-04 21:57:26 +0500

    Admin Pannel

--- lib/src/screens/admin_pannel/admin_pannel.dart
+++ lib/src/screens/admin_pannel/admin_pannel.dart
@@ -4,9 +4,13 @@ import 'dart:io';
 
 import 'package:diet_suggestion_app/src/model/item.dart';
 import 'package:diet_suggestion_app/src/screens/home/home_screen.dart';
+import 'package:diet_suggestion_app/src/widget/detail_vitamin.dart';
+import 'package:filter_list/filter_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:palette_generator/palette_generator.dart';
 
 class AdminPanel extends StatefulWidget {
   @override
@@ -15,6 +19,8 @@ class AdminPanel extends StatefulWidget {
 
 class _AdminPanelState extends State<AdminPanel> {
   File? imageFile;
+  Color color = Colors.orange;
+
   final _formKey = GlobalKey<FormState>();
   Item model = Item(
       id: 0,
@@ -39,7 +45,7 @@ class _AdminPanelState extends State<AdminPanel> {
   }
 
   List<String> label = ['Add Diet Item', 'Queries'];
-  List body() {
+  List body(BuildContext context) {
     return [
       Center(
         child: Form(
@@ -49,81 +55,173 @@ class _AdminPanelState extends State<AdminPanel> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Padding(
+                  //
                   padding: const EdgeInsets.all(8.0),
-                  child: CircleAvatar(
-                    radius: 50,
-                    backgroundColor: Colors.white,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(50),
-                      child: Image.asset(
-                        'assets/images/user.png',
-                        fit: BoxFit.cover,
-                      ),
+                  child: GestureDetector(
+                    onTap: () {
+                      showDialog(
+                          context: context,
+                          builder: (context) {
+                            return Container(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  SizedBox(
+                                    width: 200.0,
+                                    child: ElevatedButton(
+                                      onPressed: () {
+                                        _getFromGallery();
+                                      },
+                                      style: ElevatedButton.styleFrom(
+                                        primary: Colors.white,
+                                      ),
+                                      child: const Text(
+                                        "Gallery",
+                                        style: TextStyle(color: Colors.black),
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 200.0,
+                                    child: ElevatedButton(
+                                      onPressed: () {
+                                        _getFromCamera();
+                                      },
+                                      style: ElevatedButton.styleFrom(
+                                        primary: Colors.white,
+                                      ),
+                                      child: const Text(
+                                        "Camera",
+                                        style: TextStyle(color: Colors.black),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            );
+                          });
+                    },
+                    child: Stack(
+                      children: [
+                        CircleAvatar(
+                          radius: 50,
+                          backgroundColor: Colors.white,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(50),
+                            child: imageFile == null
+                                ? Image.asset(
+                                    'assets/images/user.png',
+                                    fit: BoxFit.cover,
+                                  )
+                                : SizedBox(
+                                    child: Image.file(
+                                      imageFile!,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                          ),
+                        ),
+                        Positioned(
+                          right: 0.0,
+                          bottom: 0.0,
+                          child: Icon(
+                            Icons.camera,
+                            color: Colors.orange,
+                            size: 30.0,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
+
                 MyTextFormField(
                   hintText: 'Name',
                 ),
-                SizedBox(
-                  width: 300.0,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      _getFromGallery();
-                    },
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.white,
-                    ),
-                    child: const Text(
-                      "Pick Image From Gallery",
-                      style: TextStyle(color: Colors.black),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width: 300.0,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      _getFromCamera();
-                    },
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.white,
-                    ),
-                    child: const Text(
-                      "Pick Image From Camera",
-                      style: TextStyle(color: Colors.black),
-                    ),
-                  ),
-                ),
+
                 MyTextFormField(
                   hintText: 'Rating',
                 ),
-                // MyTextFormField(
-                //   hintText: 'ratingCount',
-                //   // isPassword: true,
-                // ),
                 MyTextFormField(
                   hintText: 'Price',
-                  // isPassword: true,
                 ),
-                // MyTextFormField(
-                //   hintText: 'color',
-                //   // isPassword: true,
-                // ), also wee  will try to add color picker
-                MyTextFormField(
+               MyTextFormField(
                   hintText: 'Description', //
                   line: 3,
-                  // isEmail: true, // or ui me symmetry mean HCI concepts ka kheyal rkha kro yeh kro then baki dekhte
                 ),
-                MyTextFormField(
-                  hintText: 'Vitamins',
-                  line: 3,
-                  // isPassword: true,
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    onTap: () {
+                      openFilterDialog();
+                    },
+                    readOnly: true,
+                    decoration: InputDecoration(
+                      hintText: selectedList.toString(),
+                      contentPadding: const EdgeInsets.all(15.0),
+                      border: InputBorder.none,
+                      filled: true,
+                      fillColor: Colors.grey[200],
+                    ),
+                  ),
                 ),
-                MyTextFormField(
-                  hintText: 'Ingredients',
-                  line: 3,
-                  // isPassword: true,
+
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: DropdownButtonFormField(
+                      decoration: InputDecoration(
+                        hintText: "Ingredients",
+                        contentPadding: const EdgeInsets.all(15.0),
+                        border: InputBorder.none,
+                        filled: true,
+                        fillColor: Colors.grey[200],
+                      ),
+                      value: "1",
+                      items: [
+                        DropdownMenuItem(
+                          child: Row(
+                            children: [
+                              SvgPicture.asset("assets/icons/ingredient1.svg"),
+                              SizedBox(width: 20.0),
+                              Text("Apple"),
+                            ],
+                          ),
+                          value: "1",
+                        ),
+                        DropdownMenuItem(
+                          child: Row(
+                            children: [
+                              SvgPicture.asset("assets/icons/ingredient2.svg"),
+                              SizedBox(width: 20.0),
+                              const Text("Avocado"),
+                            ],
+                          ),
+                          value: "2",
+                        ),
+                        DropdownMenuItem(
+                          child: Row(
+                            children: [
+                              SvgPicture.asset("assets/icons/ingredient3.svg"),
+                              SizedBox(width: 20.0),
+                              Text("Orange"),
+                            ],
+                          ),
+                          value: "3",
+                        ),
+                        DropdownMenuItem(
+                          child: Row(
+                            children: [
+                              SvgPicture.asset("assets/icons/ingredient4.svg"),
+                              SizedBox(width: 20.0),
+                              Text("Strawberry"),
+                            ],
+                          ),
+                          value: "4",
+                        ),
+                      ],
+                      onChanged: (value) {
+                        print(value);
+                      }),
                 ),
                 RaisedButton(
                   color: Colors.blueAccent,
@@ -166,6 +264,34 @@ class _AdminPanelState extends State<AdminPanel> {
     ];
   }
 
+  int tag = 1;
+  List<String> options = ['A', 'B', 'C', 'D', 'E'];
+  List<String> selectedList = [];
+  void openFilterDialog() async {
+    await FilterListDialog.display<String>(
+      context,
+      listData: options,
+      selectedListData: selectedList,
+      choiceChipLabel: (opt) => opt,
+      validateSelectedItem: (list, val) => list!.contains(val),
+      onItemSearch: (opt, query) {
+        return opt.toLowerCase().contains(query.toLowerCase());
+      },
+      onApplyButtonClick: (list) {
+        setState(() {
+          selectedList = List.from(list!);
+        });
+        Navigator.pop(context);
+      }, 
+    );
+  }
+
+  Future<Color> getImagePalette(ImageProvider imageProvider) async {
+    final PaletteGenerator paletteGenerator =
+        await PaletteGenerator.fromImageProvider(imageProvider);
+    return paletteGenerator.dominantColor!.color;
+  }
+
   _getFromGallery() async {
     PickedFile? pickedFile = await ImagePicker().getImage(
       source: ImageSource.gallery,
@@ -178,6 +304,8 @@ class _AdminPanelState extends State<AdminPanel> {
           imageFile = File(pickedFile.path);
         },
       );
+      color = await getImagePalette(FileImage(imageFile!));
+      setState(() {});
     }
   }
 
@@ -193,6 +321,8 @@ class _AdminPanelState extends State<AdminPanel> {
           imageFile = File(pickedFile.path);
         },
       );
+      color = await getImagePalette(FileImage(imageFile!));
+      setState(() {});
     }
   }
 
@@ -222,7 +352,7 @@ class _AdminPanelState extends State<AdminPanel> {
         centerTitle: true,
         backgroundColor: const Color.fromARGB(255, 52, 52, 52),
       ),
-      body: body()[_selectedIndex],
+      body: body(context)[_selectedIndex],
     );
   }
 }
